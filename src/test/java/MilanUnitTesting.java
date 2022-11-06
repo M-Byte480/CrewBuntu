a0afc7d5c1309208e47bad19d486234563d00bf6
M-Byte480 <77645654+M-Byte480@users.noreply.github.com>
2022-11-06 03:44:01 +0000

    testing this stupid yoke

--- src/test/java/MilanUnitTesting.java
+++ src/test/java/MilanUnitTesting.java
@@ -1,13 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.*;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
-import org.mockito.junit.jupiter.*;
-
-import java.util.List;
-
-@ExtendWith(MockitoExtension.class)
-public class MilanUnitTesting {
-
-}
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.*;
+//import org.junit.jupiter.api.*;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.*;
+//import org.mockito.junit.jupiter.*;
+//
+//import java.util.List;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class MilanUnitTesting {
+//
+//}
