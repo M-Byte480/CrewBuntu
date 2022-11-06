ee36e2259dfa77c08bd14ce979cf4bf820cab650
M-Byte480 <77645654+M-Byte480@users.noreply.github.com>
2022-11-06 03:52:14 +0000

    testing this stupid yoke

--- src/test/java/MilanUnitTesting.java
+++ src/test/java/MilanUnitTesting.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MilanUnitTesting {
     @Test
     void testBook(){
-        assertTrue(false);
+        assertEquals(213, 233);
     }
 }
